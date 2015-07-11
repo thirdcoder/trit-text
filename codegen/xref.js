dcc234eaf2e6c4872274080bd8afbac5fa06611e
thirdcoder <boris3rd@bk.ru>
2015-07-10 23:46:36 -0700

    Add generated bitmaps for trit-text codepoints from CP437 9x14
    cd codegen
    node xref.js  > ../vtext.js

--- codegen/xref.js
+++ codegen/xref.js
@@ -21,8 +21,37 @@ for (var i in trittext) {
   }
 }
 
+/*
 console.log('('+unassigned.length+') cp437 - trittext = ');
 console.log(unassigned.join(''));
 console.log();
 console.log('('+extra.length+') trittext - cp437 = ');
 console.log(extra);
+*/
+
+console.log('module.exports={');
+for (var i in trittext) {
+  var c = trittext[i];
+
+  var s='';
+  var cp = c.charCodeAt(0);
+  if(cp===39)s="'\\''";
+  else if(cp===0)s="'\\u0000'";
+  else if(cp==92)s="'\\\\'";
+  else if(cp==10)s="'\\\\n'";
+  else s+="'"+c+"'";
+  console.log(s+':');
+
+  if (cp437[c]) {
+    var lines = cp437[c].split('\n');
+    for (var i=0;i<lines.length-1;++i) {
+      var line=lines[i];
+      console.log("'" + line + "\\n'"+(i==13?',':'+'));
+    }
+  } else {
+    console.log('"", // TODO');
+  }
+  console.log();
+}
+console.log('};');
+
