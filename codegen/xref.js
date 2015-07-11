e158f4d8d410493b9e13f73f9985f8b55af6234f
thirdcoder <boris3rd@bk.ru>
2015-07-10 23:30:31 -0700

    Show symmetric difference cp437 - trittext and vice versa
    (138) cp437 - trittext =
    ♪♫↕‼¶§▬↨∟↔⌂ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜ¢£¥₧ƒáíóúñÑªº¿⌐¬½¼¡«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αßΓπΣσµτΦΘΩδ∞φε∩≡±≥≤⌠⌡÷°∙·√ⁿ²■ 
    
    (4) trittext - cp437 =
    [ '�', '\n', '‾', '☂' ]

--- codegen/xref.js
+++ codegen/xref.js
@@ -2,6 +2,27 @@
 
 var trittext = require('../').allUnicode;
 var cp437 = require('./cp437-bitmaps');
+
+// find in CP437 but not trit-text
+var unassigned = [];
 for (var c in cp437) {
-  console.log(c, trittext.indexOf(c));
+  if (trittext.indexOf(c) === -1) {
+    unassigned.push(c);
+  }
+  //console.log(c, trittext.indexOf(c));
 }
+
+// find in trit-text but not CP437
+var extra = [];
+for (var i in trittext) {
+  var c = trittext[i];
+  if (!cp437[c]) {
+    extra.push(c);
+  }
+}
+
+console.log('('+unassigned.length+') cp437 - trittext = ');
+console.log(unassigned.join(''));
+console.log();
+console.log('('+extra.length+') trittext - cp437 = ');
+console.log(extra);
