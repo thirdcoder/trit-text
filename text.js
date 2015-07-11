96a6774f767686fea76f8b478231c56e88e05786
thirdcoder <boris3rd@bk.ru>
2015-07-11 00:01:42 -0700

    Add API to get a 9x14 tritmap (~bitmap) of a character

--- text.js
+++ text.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _vtext = require('./vtext');
+
 /*
 5-trit text format, analogous to 7-bit ASCII
 
@@ -157,9 +159,38 @@ function fromUnicode(c, inverted) {
   return n;
 }
 
+function toTritmap(n) {
+  var inverted = isInverted(n);
+  var unicode = toUnicode(n);
+  if (unicode === undefined) throw new Error('no character for trit-text codepoint:'+n);
+  var vtext = _vtext[unicode];
+  if (!vtext) throw new Error('no tritmap for character: '+n);
+
+  var tritmap = '';
+
+  for (var i = 0; i < vtext.length; ++i) {
+    // for source code readability, the tritmap is stored encoded TODO: store converted if too slow
+    switch(vtext.charAt(i)) {
+      case '\n':
+        break;
+      case '.':
+        tritmap += '0';
+        break;
+      case '*':
+        tritmap += inverted ? 'i' : '1';
+        break;
+      default:
+        throw new Error('invalid vtext data for tritmap character '+n+': '+vtext.charAt(i));
+      }
+  }
+
+  return tritmap;
+}
+
 module.exports = {
   toUnicode: toUnicode,
   isInverted: isInverted,
   fromUnicode: fromUnicode,
   allUnicode: _chars,
+  toTritmap9x14: toTritmap,
 };
