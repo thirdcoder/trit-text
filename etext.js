f67ffeb55beaf60dab521c8f2436bfdc807b6230
thirdcoder <boris3rd@bk.ru>
2015-07-11 15:04:26 -0700

    Add a keymap for translating keyboard keycodes

--- etext.js
+++ etext.js
@@ -3,6 +3,61 @@
 var vkey = require('vkey');
 var fromUnicode = require('./utext').fromUnicode;
 
+// map virtual key to Unicode corresponding to trit-text, with modifiers
+var _keymap = {
+  // vkey: normal, shifted, control, shift-control
+  A: ['a', 'A', '', ''],
+  B: ['b', 'B', '', ''],
+  C: ['c', 'C', '', ''],
+  D: ['d', 'D', '', ''],
+  E: ['e', 'E', '', ''],
+  F: ['f', 'F', '', ''],
+  G: ['g', 'G', '', ''],
+  H: ['h', 'H', '', ''],
+  I: ['i', 'I', '', ''],
+  J: ['j', 'J', '', ''],
+  K: ['k', 'K', '', ''],
+  L: ['l', 'L', '', ''],
+  M: ['m', 'M', '', ''],
+  N: ['n', 'N', '', ''],
+  O: ['o', 'O', '', ''],
+  P: ['p', 'P', '', ''],
+  Q: ['q', 'Q', '', ''],
+  R: ['r', 'R', '', ''],
+  S: ['s', 'S', '', ''],
+  T: ['t', 'T', '', ''],
+  U: ['u', 'U', '', ''],
+  V: ['v', 'V', '', ''],
+  W: ['w', 'W', '', ''],
+  X: ['x', 'X', '', ''],
+  Y: ['y', 'Y', '', ''],
+  Z: ['z', 'Z', '', ''],
+
+  '`': ['`', '~', '', ''],
+  1: ['1', '!', '', ''],
+  2: ['2', '@', '', ''],
+  3: ['3', '#', '', ''],
+  4: ['4', '$', '', ''],
+  5: ['5', '%', '', ''],
+  6: ['6', '^', '', ''],
+  7: ['7', '&', '', ''],
+  8: ['8', '*', '', ''],
+  9: ['9', '(', '', ''],
+  0: ['0', ')', '', ''],
+  '-': ['-', '_', '', ''],
+  '=': ['=', '+', '', ''],
+  '[': ['[', '{', '', ''],
+  ']': [']', '}', '', ''],
+  '\\': ['\\', '|', '', ''],
+  ';': [';', ':', '', ''],
+  '\'': ['\\', '"', '', ''],
+  ',': [',', '<', '', ''],
+  '.': ['.', '>', '', ''],
+  '/': ['/', '?', '', ''],
+
+  '<enter>': ['\n', '\n', '', ''],
+};
+
 // option-keys to press for shortcuts
 var _sc = {};
 if(0){//TODO
@@ -20,19 +75,13 @@ _sc[OPT.S] = '◄'; _sc[OPT.T] = '►';
 }
 
 function fromEvent(ev) {
-  var c = vkey[ev.keyCode];
+  var vkeyChar = vkey[ev.keyCode];
 
-  if (c === '<enter>') {
-    return 12; // trit-text newline
-  }
+  var flags = (ev.shiftKey ? 1 : 0) + (ev.ctrlKey ? 2 : 0); // TODO: metaKey(cmd)? altKey(opt)?
 
-  if (!ev.shiftKey) {
-    c = c.toLowerCase();
-  };
-
-  if (_sc[c] !== undefined) {
-    c = _sc[c];
-  }
+  var varChars = _keymap[vkeyChar];
+  if (!varChars) return null;
+  var c = varChars[flags];
 
   var tt = fromUnicode(c);
   if (tt == null) return null;
